94782c689ecd65463517725d602dff3212df4e36
Enock <enock.mokua@student.moringaschool.com>
2023-03-22 08:59:57 +0300

    craete eployer controller methods

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,2 +1,16 @@
 class EmployersController < ApplicationController
+  def index
+    render json: Employer.all
+  end
+
+  def create
+    employer = Employer.create(employer_params)
+    render json: employer
+  end
+
+  private
+
+  def employer_params
+    params.permit(:name)
+  end
 end
