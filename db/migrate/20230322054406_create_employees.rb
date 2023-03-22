01a9b10df72cc560acdc55b3b54fb860e27cb639
Enock <enock.mokua@student.moringaschool.com>
2023-03-22 08:48:21 +0300

    create attrinutes for employer and employee models

--- db/migrate/20230322054406_create_employees.rb
+++ db/migrate/20230322054406_create_employees.rb
@@ -2,6 +2,7 @@ class CreateEmployees < ActiveRecord::Migration[7.0]
   def change
     create_table :employees do |t|
       t.references :employer, null: false, foreign_key: true
+      t.string :name
 
       t.timestamps
     end
