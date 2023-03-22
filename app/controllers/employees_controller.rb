0797c56cba5f6ad6e7f3a73498946c16b3f77936
Enock <enock.mokua@student.moringaschool.com>
2023-03-22 08:56:51 +0300

    create controller methods for employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,17 @@
 class EmployeesController < ApplicationController
+  def index
+    employees = Employee.all
+    render json: employees
+  end
+
+  def create
+    employee = Employee.create(employee_params)
+    render json: employee
+  end
+
+  private
+
+  def employee_params
+    params.permit(:employer_id, :name)
+  end
 end
