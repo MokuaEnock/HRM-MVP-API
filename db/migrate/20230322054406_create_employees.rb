f296777f1ef4c8062124221631667e856ae9e631
Enock <enock.mokua@student.moringaschool.com>
2023-03-22 08:44:20 +0300

    regenerate eployee odel

--- db/migrate/20230322054406_create_employees.rb
+++ db/migrate/20230322054406_create_employees.rb
@@ -1,6 +1,7 @@
 class CreateEmployees < ActiveRecord::Migration[7.0]
   def change
     create_table :employees do |t|
+      t.references :employer, null: false, foreign_key: true
 
       t.timestamps
     end
