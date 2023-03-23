c97598732ccb63763be2f064c51940c656d00def
Enock <enock.mokua@student.moringaschool.com>
2023-03-23 15:50:46 +0300

    create attricutes for employee bank

--- db/migrate/20230323114843_create_employeebanks.rb
+++ db/migrate/20230323114843_create_employeebanks.rb
@@ -3,6 +3,12 @@ class CreateEmployeebanks < ActiveRecord::Migration[7.0]
     create_table :employeebanks do |t|
       t.references :employee, null: false, foreign_key: true
 
+      t.string :bank_name
+      t.string :branch_name
+      t.string :account_name
+      t.string :bank_code
+      t.string :branch_code 
+
       t.timestamps
     end
   end
