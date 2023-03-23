5c405cc7ca5d19675a7c05d1019c379b5ecd4f46
Enock <enock.mokua@student.moringaschool.com>
2023-03-23 17:33:42 +0300

    create employer financials and bank details

--- app/controllers/employerfinancials_controller.rb
+++ app/controllers/employerfinancials_controller.rb
@@ -1,2 +1,34 @@
 class EmployerfinancialsController < ApplicationController
+  def create
+    schedule = Employerfinancial.create(sch_params)
+    render json: schedule
+  end
+
+  def show
+    schedule = Employerfinancial.find(params[:id])
+    render json: schedule
+  end
+
+  def index
+    schedules = Employerfinancial.all
+    render json: schedules
+  end
+
+  def destroy
+    schedule = Employerfinancial.find(params[:id])
+    schedule.destroy
+    render json: schedule
+  end
+
+  def update
+    schedule = Employerfinancial.find(params[:id])
+    schedule.update(sch_params)
+    render json: schedule
+  end
+
+  private
+
+  def sch_params
+    params.permit(:nhif_number, :nssf_number, :kra_pin)
+  end
 end
--- db/migrate/20230323032452_create_employerfinancials.rb
+++ db/migrate/20230323032452_create_employerfinancials.rb
@@ -2,7 +2,10 @@ class CreateEmployerfinancials < ActiveRecord::Migration[7.0]
   def change
     create_table :employerfinancials do |t|
       t.references :employer, null: false, foreign_key: true
-      t.string :name
+
+      t.string :nhif_number
+      t.string :nssf_number
+      t.string :kra_pin
 
       t.timestamps
     end
