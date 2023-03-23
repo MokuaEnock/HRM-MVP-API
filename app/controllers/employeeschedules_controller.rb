3feef012a897a1fc287e4e0c5e9b8e8555a40824
Enock <enock.mokua@student.moringaschool.com>
2023-03-23 16:28:11 +0300

    create  schedules methods

--- app/controllers/employeeschedules_controller.rb
+++ app/controllers/employeeschedules_controller.rb
@@ -1,2 +1,22 @@
 class EmployeeschedulesController < ApplicationController
+  def create
+    schedule = Employeeschedule.create(sch_params)
+    render json: schedule
+  end
+
+  def show
+  end
+
+  def index
+    render json: Employeeschedule.all
+  end
+
+  def destroy
+  end
+
+  private
+
+  def sch_params
+    params.permit(:name, :description, :date, :time, :employee_id)
+  end
 end
