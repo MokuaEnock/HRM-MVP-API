cfc17e80bbc0970005798b18932c0def2d3a48f5
Enock <enock.mokua@student.moringaschool.com>
2023-03-23 17:10:43 +0300

    create methods for employer bank

--- app/controllers/employerbanks_controller.rb
+++ app/controllers/employerbanks_controller.rb
@@ -1,2 +1,28 @@
 class EmployerbanksController < ApplicationController
+  def create
+    schedule = Employeeschedule.create(sch_params)
+    render json: schedule
+  end
+
+  def show
+    schedule = Employeeschedule.find(params[:id])
+    render json: schedule
+  end
+
+  def index
+    schedules = Employeeschedule.all
+    render json: schedules
+  end
+
+  def destroy
+    schedule = Employeeschedule.find(params[:id])
+    schedule.destroy
+    render json: schedule
+  end
+
+  private
+
+  def sch_params
+    params.permit(:name, :description, :date, :time, :employee_id)
+  end
 end
