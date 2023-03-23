8038d64b9667cb93ac919ad4e5c89316e11bf486
Enock <enock.mokua@student.moringaschool.com>
2023-03-23 13:54:44 +0300

    create department controller actions

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,2 +1,20 @@
 class DepartmentsController < ApplicationController
+  def create
+    depts = Department.create(dep_params)
+    render json: depts
+  end
+
+  def index
+    render json: Department.all
+  end
+
+  def show
+    render json: Department.find(params[:id]), status: :ok
+  end
+
+  private
+
+  def dep_params
+    permit.params(:name, :employer_id)
+  end
 end
--- db/migrate/20230323103230_create_departments.rb
+++ db/migrate/20230323103230_create_departments.rb
@@ -2,6 +2,7 @@ class CreateDepartments < ActiveRecord::Migration[7.0]
   def change
     create_table :departments do |t|
       t.references :employer, null: false, foreign_key: true
+      t.string :name
 
       t.timestamps
     end
