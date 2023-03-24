2028e39ecf07fb819504aed1364b3ca154bd09d6
Enock <enock.mokua@student.moringaschool.com>
2023-03-24 22:46:15 +0300

    update attendance methdos

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,36 +1,46 @@
 class AttendancesController < ApplicationController
-  before_action :set_employee
-  before_action :set_attendance, only: [:show, :update, :destroy]
+  # before_action :set_employee
+  # before_action :set_attendance, only: [:show, :update, :destroy]
 
   def index
-    @attendance = @employee.attendance.all
-    render json: @attendance
+    render json: {}
   end
 
   def create
-    @attendance = @employee
+    attendace = Attenndance.create(att_params)
+    render json: attendace
   end
 
   def show
-    render json: @attendance
+    attendace = Attendance.find(params[:id])
+    render json: attendace
   end
 
   def update
+    attendence = Attendance.find(params[:id])
+
+    if attendence.update(user_params)
+      render json: { message: "User updated successfully" }, status: :ok
+    else
+      render json: { errors: attendence.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    @attendance.destroy
+    attendance = Attendance.find(params[:id])
+    attendance.destroy
+    render json: { message: "Attendance deleted" }, status: :ok
   end
 
   private
 
-  def set_employee
-    @employee = Employee.find(params[:employee_id])
-  end
+  # def set_employee
+  #   @employee = Employee.find(params[:employee_id])
+  # end
 
-  def set_attendance
-    @attendance = @employee.attendances.find(params[:id])
-  end
+  # def set_attendance
+  #   @attendance = @employee.attendances.find(params[:id])
+  # end
 
   def att_params
     params.permit(:date, :department_id, :employee_id, :time_in, :time_out, :is_present, :is_late, :reason)
