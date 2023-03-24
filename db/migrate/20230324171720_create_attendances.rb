cf5fa43fb5c37fd469ce05bd67c361fec835612f
Enock <enock.mokua@student.moringaschool.com>
2023-03-24 20:36:54 +0300

    create attributes for attendace

--- db/migrate/20230324171720_create_attendances.rb
+++ db/migrate/20230324171720_create_attendances.rb
@@ -2,6 +2,14 @@ class CreateAttendances < ActiveRecord::Migration[7.0]
   def change
     create_table :attendances do |t|
       t.references :employee, null: false, foreign_key: true
+      t.references :department, null: false, foreign_key: true
+
+      t.datetime :date
+      t.datetime :time_in
+      t.datetime :time_out
+      t.boolean :is_present
+      t.boolean :is_late
+      t.boolean :reason
 
       t.timestamps
     end
