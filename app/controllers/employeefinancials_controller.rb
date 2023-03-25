d4b7fd147ff8d5a641938694d8b7a982f378b2ae
Enock <enock.mokua@student.moringaschool.com>
2023-03-25 10:47:41 +0300

    create methods for employee financials

--- app/controllers/employeefinancials_controller.rb
+++ app/controllers/employeefinancials_controller.rb
@@ -1,2 +1,22 @@
 class EmployeefinancialsController < ApplicationController
+  def index
+    financials = Employeefinancial.all
+    render json: financials
+  end
+
+  def create
+    financial = Employeefinancial.create(fin_params)
+    render json: financial
+  end
+
+  def show
+    financial = Employeefinancial.find(params[:id])
+    render json: financial
+  end
+
+  private
+
+  def fin_params
+    params.permit(:nssf_number, :nhif_number, :kra_pin, :employee_id)
+  end
 end
