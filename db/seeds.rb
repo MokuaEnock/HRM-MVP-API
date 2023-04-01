5414ba6c5b9e555feaaf9d44b82943afb97fab33
Enock <enock.mokua@student.moringaschool.com>
2023-04-02 00:48:02 +0300

    add seed data for attendace

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,4 +18,21 @@ departments = Department.create([{ employer_id: 1, email: "engineering@gmail.com
 employees = Employee.create([{ department_id: 1, email: "mokua@enock.com", employee_number: "zab3", password: "enock", password_confirmation: "enock" },
                              { department_id: 1, email: "leila@kem.com", employee_number: "kem2", password: "enock", password_confirmation: "enock" }])
 
+# Create an array of all the dates from January 1st until today, excluding Sundays and occasional Saturdays
+dates = (Date.parse("2022-01-01")..Date.today).select { |date| date.on_weekday? || (date.saturday? && rand(10) < 3) }
+
+# Create seed data for each date for the two employees
+dates.each do |date|
+  Employee.all.each do |employee|
+    # Randomly generate time_in and time_out values within the workday hours
+    time_in = date + rand(8..9).hours
+    time_out = date + rand(17..18).hours
+    # Create a new attendance record with the employee_id, date, time_in, and time_out
+    attendance = Attendance.new(employee_id: employee.id, date: date, time_in: time_in, time_out: time_out)
+    attendance.calculate_total_worked_hours
+    attendance.calculate_pay
+    attendance.save
+  end
+end
+
 puts "done seeding"
