acb9f5f9749ea49ca6b09bfeb148b57affcb94a2
Enock <enock.mokua@student.moringaschool.com>
2023-04-01 21:57:33 +0300

    generate the columns for attendance and class

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,3 +1,34 @@
 class Attendance < ApplicationRecord
   belongs_to :employee
+
+  validates :date, presence: true
+  validates :clock_in, presence: true
+  validates :clock_out, presence: true
+  validates :employee_id, presence: true
+
+  before_save :calculate_pay, :calculate_total_worked_hours
+
+  private
+
+  def calculate_pay
+    #calculate pay for weekdays
+    if date.wday.between?(1, 5) && worked_hours > 0 && worked_hours <= 8
+      self.pay = worked_hours * 772 #772/8 hours = 96.5 per hour
+    end
+
+    #calculate pay for weekends
+    if date.wday.between?(6, 7) && worked_hours > 0 && worked_hours <= 8
+      self.pay = worked_hours * 193 #772/4 hours = 193 per hour (double pay)
+    end
+  end
+
+  def calculate_total_worked_hours
+    self.total_worked_hours = worked_hours.round(2)
+  end
+
+  def worked_hours
+    hours = clock_out.hour - clock_in.hour
+    minutes = (clock_out.min - clock_in.min) / 60.0
+    hours + minutes
+  end
 end
--- db/migrate/20230324171720_create_attendances.rb
+++ db/migrate/20230324171720_create_attendances.rb
@@ -3,12 +3,12 @@ class CreateAttendances < ActiveRecord::Migration[7.0]
     create_table :attendances do |t|
       t.references :employee, null: false, foreign_key: true
 
-      t.decimal :total_worked_hours, precision: 10, scale: 2
-      t.decimal :pay, precision: 10, scale: 2
+      t.decimal :total_worked_hours, precision: 10, scale: 2, default: 0.0, null: false
+      t.decimal :pay, precision: 10, scale: 2, default: 0.0, null: false
       t.string :reason
-      t.string :time_in
-      t.string :time_out
-      t.string :date
+      t.time :clock_in
+      t.time :clock_out
+      t.date :date
 
       t.timestamps
     end
