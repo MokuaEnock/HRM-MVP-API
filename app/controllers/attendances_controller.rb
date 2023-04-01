3ebda0f58625284cbd8769d7beb506dd5ace9d37
Enock <enock.mokua@student.moringaschool.com>
2023-04-01 23:27:53 +0300

    reedit the attendace controller and add attributes to it

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,32 +1,32 @@
 class AttendancesController < ApplicationController
-  # GET /time_sheets
+
+  # GET /attendances
   def index
     # Retrieve all time sheets from the database, including the associated employee records
     @attendances = Attendance.includes(:employee).all
     render json: @attendances
   end
 
-  # POST /time_sheets
+  # POST /attendances
   def create
     # Instantiate a new Attendance object with the given parameters
-    @attendance = Attendance.new(time_sheet_params)
+    attendance = Attendance.new(att_params)
 
     # Calculate the total worked hours and pay for this time sheet
-    @attendance.calculate_total_worked_hours
-    @attendance.calculate_pay
+    attendance.calculate_total_worked_hours
+    attendance.calculate_pay
 
     # Save the time sheet to the database
-    if @attendance.save
-      render json: @attendance, status: :created
+    if attendance.save
+      render json: attendance, status: :created
     else
-      render json: @attendance.errors, status: :unprocessable_entity
+      render json: attendance.errors, status: :unprocessable_entity
     end
   end
 
   private
 
-  # Only allow a trusted parameter "white list" through
-  def time_sheet_params
-    params.require(:time_sheet).permit(:employee_id, :date, :time_in, :time_out)
+  def att_params
+    params.require(:attendance).permit(:employee_id, :date, :time_in, :time_out)
   end
 end
