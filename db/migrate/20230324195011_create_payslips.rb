75644e2e8b3d8b903172f7f239b33f329c664015
Enock <enock.mokua@student.moringaschool.com>
2023-04-02 07:17:47 +0300

    create attributes for payslip model

--- db/migrate/20230324195011_create_payslips.rb
+++ db/migrate/20230324195011_create_payslips.rb
@@ -2,14 +2,13 @@ class CreatePayslips < ActiveRecord::Migration[7.0]
   def change
     create_table :payslips do |t|
       t.references :employee, null: false, foreign_key: true
-      
-      t.date :month
-      t.integer :basic_pay
-      t.integer :deductions, default: 0
-      t.integer :paye, default: 0
-      t.integer :nhif, default: 0
-      t.integer :nssf, default: 0
-      t.integer :net_pay, default: 0
+
+      t.date :start_date, null: false
+      t.date :end_date, null: false
+      t.decimal :nhif, precision: 10, scale: 2, null: false
+      t.decimal :nssf, precision: 10, scale: 2, null: false
+      t.decimal :paye, precision: 10, scale: 2, null: false
+      t.number :payslip_period
 
       t.timestamps
     end
