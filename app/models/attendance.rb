d10b09d9f75a9769b61799ee669a6906b736d1bf
Enock <enock.mokua@student.moringaschool.com>
2023-04-03 00:48:51 +0300

    create attendance sumarry foremployee

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -40,4 +40,36 @@ class Attendance < ApplicationRecord
   def weekend?
     date.on_weekend?
   end
+
+  def self.attendance_summary_for_employee(employee_id)
+    # Get all attendances for the employee
+    attendances = where(employee_id: employee_id)
+
+    # Group attendances by month
+    attendances_by_month = attendances.group_by { |attendance| attendance.date.month }
+
+    # Calculate attendance summary for each month
+    attendance_summary_by_month = {}
+    attendances_by_month.each do |month, attendances|
+      # Get the first and last day of the month
+      year = attendances.first.date.year
+      start_date = Date.new(year, month, 1)
+      end_date = start_date.end_of_month
+
+      # Calculate total days present and absent, and total pay received
+      total_present_days = attendances.count { |attendance| attendance.total_worked_hours > 0 && attendance.date.between?(start_date, end_date) }
+      total_absent_days = end_date.day - total_present_days
+      total_pay_received = attendances.sum(:pay)
+
+      # Add the attendance summary to the hash
+      attendance_summary_by_month[month] = {
+        present_days: total_present_days,
+        absent_days: total_absent_days,
+        total_pay: total_pay_received,
+      }
+    end
+
+    # Return the attendance summary for all months
+    attendance_summary_by_month
+  end
 end
