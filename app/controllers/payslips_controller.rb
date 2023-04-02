d330bd44aad953239f3bac8ad8d28c4e2ac815c9
Enock <enock.mokua@student.moringaschool.com>
2023-04-02 07:24:26 +0300

    create template for payslips controller

--- app/controllers/payslips_controller.rb
+++ app/controllers/payslips_controller.rb
@@ -1,2 +1,16 @@
 class PayslipsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+
+  def pay_params
+    params.permit(:employee_id, :start_date, :end_date)
+  end
 end
