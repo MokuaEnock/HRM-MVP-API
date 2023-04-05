26fd88765b9aed9649ab50c769c33949c67d5edd
Enock <enock.mokua@student.moringaschool.com>
2023-04-05 18:02:54 +0300

    create employeeworks controller

--- app/controllers/employeeworks_controller.rb
+++ app/controllers/employeeworks_controller.rb
@@ -1,2 +1,21 @@
 class EmployeeworksController < ApplicationController
+  def index
+    render json: Employeework.all
+  end
+
+  def show
+    works = Employeework.find(params[:id])
+    render json: works
+  end
+
+  def create
+    work = Employeework.create(emp_params)
+    render json: work
+  end
+
+  private
+
+  def emp_params
+    params.permit(:employee_id, :basic_salary, :employee_role, :employee_number, :employee_job_group)
+  end
 end
