068979bb50acafac00c854f6d657f15269a86b14
Enock <enock.mokua@student.moringaschool.com>
2023-04-06 17:17:03 +0300

    add overtie column to attendance model

--- db/migrate/20230324171720_create_attendances.rb
+++ db/migrate/20230324171720_create_attendances.rb
@@ -4,6 +4,7 @@ class CreateAttendances < ActiveRecord::Migration[7.0]
       t.references :employee, null: false, foreign_key: true
 
       t.decimal :total_worked_hours, precision: 10, scale: 2, default: 0.0, null: false
+      t.decimal :overtime, precision: 10, scale: 2, default: 0.0, null: false
       t.decimal :pay, precision: 10, scale: 2, default: 0.0, null: false
       t.string :reason
       t.time :time_in
