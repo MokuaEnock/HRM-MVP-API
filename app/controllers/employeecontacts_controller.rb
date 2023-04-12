a1d9b6ece9b660efc219e83f334207cb03b27f63
Enock <enock.mokua@student.moringaschool.com>
2023-04-12 12:12:29 +0300

    create employee contact controller

--- app/controllers/employeecontacts_controller.rb
+++ app/controllers/employeecontacts_controller.rb
@@ -1,2 +1,26 @@
 class EmployeecontactsController < ApplicationController
+  def index
+    contact = Employeecontact.all
+    render json: contact
+  end
+
+  def create
+    contact = Employeecontact.new(cont_params)
+    if contact.save
+      render json: contact, status: :created
+    else
+      render json: { errors: contact.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    contact = Employeecontact.find(params[:id])
+    render json: contact
+  end
+
+  private
+
+  def cont_params
+    params.permit(:employee_id, :phone_number, :email_address, :whatsapp_number)
+  end
 end
