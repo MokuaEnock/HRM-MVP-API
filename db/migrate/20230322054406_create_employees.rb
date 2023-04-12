7fcc57176f7fbb6bd92cf490117fc2a8a5f84616
Enock <enock.mokua@student.moringaschool.com>
2023-04-12 12:26:30 +0300

    reove commneted code in eployees migration

--- db/migrate/20230322054406_create_employees.rb
+++ db/migrate/20230322054406_create_employees.rb
@@ -1,7 +1,6 @@
 class CreateEmployees < ActiveRecord::Migration[7.0]
   def change
     create_table :employees do |t|
-      # t.references :employer, null: false, foreign_key: true
       t.references :department, null: false, foreign_key: true
       t.string :email
       t.string :employee_number
