b52a6abba7a47bc82a3ca91b8d54a9718b4cd202
Enock <enock.mokua@student.moringaschool.com>
2023-04-15 23:06:45 +0300

    create attributes for discipline

--- db/migrate/20230415013641_create_disciplines.rb
+++ db/migrate/20230415013641_create_disciplines.rb
@@ -1,7 +1,17 @@
 class CreateDisciplines < ActiveRecord::Migration[7.0]
   def change
     create_table :disciplines do |t|
-      t.references :Employee, null: false, foreign_key: true
+      t.references :employee, null: false, foreign_key: true
+      t.integer :points, default: 0
+      t.string :reason
+      t.string :action_taken
+      t.boolean :verbal_warning, default: false
+      t.boolean :written_warning, default: false
+      t.boolean :suspension, default: false
+      t.boolean :termination, default: false
+      t.string :suspension_reason
+      t.datetime :suspension_start
+      t.datetime :suspension_end
 
       t.timestamps
     end
